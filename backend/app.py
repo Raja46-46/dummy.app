99345ef625c759992f1dd201cc6d7e9d9473166e
Raja46-46 <rajaumashankar004@gmail.com>
2025-08-19 12:24:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,22 +3,25 @@ import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Backend Connected ✅"
+@app.route('/')
+def home():
+    return "Hello from Backend!"
 
-@app.route("/db")
-def db_connect():
-    conn = mysql.connector.connect(
-        host="db",
-        user="root",
-        password="root",
-        database="dummydb"
-    )
-    cursor = conn.cursor()
-    cursor.execute("SELECT message FROM test;")
-    result = cursor.fetchone()
-    return f"DB Message: {result[0]}"
+@app.route('/db')
+def db_check():
+    try:
+        conn = mysql.connector.connect(
+            host="db",
+            user="root",
+            password="root",
+            database="dummydb"
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT 'Database Connected!'")
+        result = cursor.fetchone()
+        return str(result[0])
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
